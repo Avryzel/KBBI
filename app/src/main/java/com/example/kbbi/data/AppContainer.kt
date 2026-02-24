2b52b4362d311c354a15156d03dc26ff3c0202ca
Avryzel <128247566+Avryzel@users.noreply.github.com>
2026-02-24 22:51:17 +0700

    feat: implement AppContainer and Application for manual dependency injection

---
+++ app/src/main/java/com/example/kbbi/data/AppContainer.kt
@@ -0,0 +1,27 @@
+package com.example.kbbi.data
+
+import android.app.Application
+import android.content.Context
+import com.example.kbbi.data.local.KBBIDatabase
+import com.example.kbbi.data.repository.KBBIWordRepository
+import com.example.kbbi.data.repository.OfflineKBBIWordRepository
+
+interface AppContainer {
+    val kbbiWordRepository: KBBIWordRepository
+}
+
+class DefaultAppContainer(private val context: Context) : AppContainer {
+    override val kbbiWordRepository: KBBIWordRepository by lazy {
+        OfflineKBBIWordRepository(KBBIDatabase.getDatabase(context).wordDao())
+    }
+}
+
+class KBBIDataApplication : Application() {
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+
+        container = DefaultAppContainer(this)
+    }
+}
