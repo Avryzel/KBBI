7fa9de40e88c992c1b9f037d9a458545cb2faebb
Avryzel <128247566+Avryzel@users.noreply.github.com>
2026-04-02 11:12:46 +0700

    feat: set KBBIScreen as the main entry

--- app/src/main/java/com/example/kbbi/ui/KBBIScreen.kt
+++ app/src/main/java/com/example/kbbi/ui/KBBIScreen.kt
@@ -29,12 +29,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.kbbi.ui.game.KBBIViewModel
-import com.example.kbbi.ui.theme.KBBITheme
 
 @Composable
 fun KBBIScreen(viewModel: KBBIViewModel = viewModel(factory = KBBIViewModel.Factory)) {
